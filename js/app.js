daf409325b6894e4c56587ae4d2208717c16eb9e
Rubén <rubinho6@gmail.com>
2016-02-27 23:11:37 +0100

    adding internal pages

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
+function getHeight(){
+    var height = $('section.promo, section.promo-video').height();
+    $("#overlay").css({'height' : height+'px'});
+
+}
+$(function() {
+    getHeight();
+    $("#gallery a").on('click', function() {
+        var image = $(this).find('img').attr('src');
+        var imageSelected = image.split('thumbnail');
+        var fullsizeImage = imageSelected[0] + 'fullsize' + imageSelected[1];
+        $("#imageContainer").attr('src', fullsizeImage);
+    })
+})
+
+$(document).ready(function () {
+    $(window).resize(function() {
+        getHeight();
+    });
+});
+
 $(document).foundation();
